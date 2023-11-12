8a37ac6c7caa5971f73c6789ab44e54d63b586c6
Fook Athena <fookathe@su-h4.msk.21-school.ru>
2023-11-12 16:21:36 +0300

    Done 1 quest

--- src/dmanager_module.c
+++ src/dmanager_module.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h> 
-#include <time.h> 
+#include <stdlib.h>
+#include <time.h>
 
 #include "door_struct.h"
 
@@ -8,14 +8,23 @@
 #define MAX_ID_SEED 10000
 
 void initialize_doors(struct door* doors);
+void close_doors(struct door* door);
+void sort_doors(struct door* doors, int count);
 
-int main()
-{
+int main() {
     struct door doors[DOORS_COUNT];
-    
-    initialize_doors(doors);
-    
 
+    initialize_doors(doors);
+    close_doors(doors);
+    sort_doors(doors, DOORS_COUNT);
+
+    for (int i = 0; i < DOORS_COUNT; i++) {
+        if (i == DOORS_COUNT - 1) {
+            printf("%d, %d", i, doors[i].status);
+        } else {
+            printf("%d, %d\n", i, doors[i].status);
+        }
+    }
 
     return 0;
 }
@@ -23,14 +32,30 @@ int main()
 // Doors initialization function
 // ATTENTION!!!
 // DO NOT CHANGE!
-void initialize_doors(struct door* doors)
-{
-    srand(time(0)); 
-    
+void initialize_doors(struct door* doors) {
+    srand(time(0));
+
     int seed = rand() % MAX_ID_SEED;
-    for (int i = 0; i < DOORS_COUNT; i++)
-    {
+    for (int i = 0; i < DOORS_COUNT; i++) {
         doors[i].id = (i + seed) % DOORS_COUNT;
         doors[i].status = rand() % 2;
+    }
+}
+
+void close_doors(struct door* doors) {
+    for (int i = 0; i < DOORS_COUNT; i++) {
+        doors[i].status = 0;
+    }
+}
+
+void sort_doors(struct door* doors, int count) {
+    for (int i = 0; i < count - 1; i++) {
+        for (int j = 0; j < count - i - 1; j++) {
+            if (doors[j].id > doors[j + 1].id) {
+                struct door temp = doors[j];
+                doors[j] = doors[j + 1];
+                doors[j + 1] = temp;
+            }
+        }
     }
 }
